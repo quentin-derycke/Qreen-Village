e792cb9b1aca40652ad74ac4f3437c18e1cba3ba
Quentin Derycke <quentinderycke6@gmail.com>
2022-12-20 17:09:16 +0100

    💄 Address constraint

--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -9,57 +9,86 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints as Assert;
 
-class AddressType extends AbstractType 
+class AddressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('houseNumber', TextType::class, ['attr' => [
-            'class' => 'form-control mt-4',
-            
-        ],
-        'label' => 'House Number',
-        'label_attr' => ['class' => 'form-label mt-4']])
-        ->add('street', TextType::class, ['attr' => [
-            'class' => 'form-control mt-4',
-           
-        ],
-        'label' => 'Street',
-        'label_attr' => ['class' => 'form-label mt-4']])
+            ->add('houseNumber', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mt-4',
 
-        ->add('zipcode', TextType::class,  [
-            'attr' => [
-                'class' => 'form-control mt-4',
-            ],
-        'label' => 'Zipcode',
-        'label_attr' => [
-            'class' => 'form-label mt-4'
-            ]
-        ])
+                ],
+                'label' => 'House Number',
+                'label_attr' => ['class' => 'form-label mt-4'],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Regex([
+                        'pattern' => '/^\d+[a-zA-Z]?$/',
+                        'message' => ' Indiquer un Numéro valide',
+                    ])
+                ]
+            ])
+            ->add('street', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control mt-4',
 
-        ->add('city', ChoiceType::class, [
-              'attr' => [
-                'class' => 'form-control mt-4',
-            ],
-        
-        'label' => 'City',
-        'label_attr' => ['class' => 'form-label mt-4']])
-        ->addEventListener(
-            FormEvents::PRE_SUBMIT,
-            [$this, 'onPreSubmit']
-        )
-        
-    ->add('save', SubmitType::class, [
-        'attr' => ['class' => 'btn btn-primary mt-4']
-    ])
-    ->getForm();
+                ],
+                'label' => 'Street',
+                'label_attr' => ['class' => 'form-label mt-4'],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Regex([
+                        'pattern' => '/^(rue|avenue|boulevard|impasse|place|quai) \b[A-Z][a-z]*/',
+                        'message' => 'Indiquer un nom de rue en France',
+                    ])
+                ]
+            ])
+
+            ->add('zipcode', TextType::class,  [
+                'attr' => [
+                    'class' => 'form-control mt-4',
+                ],
+                'label' => 'Zipcode',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Regex([
+                        'pattern' => '/^[0-9]{5}$/',
+                        'message' => 'Indiquer un Code postal Français',
+                    ])
+                ]
+            ])
+
+            ->add('city', ChoiceType::class, [
+                'attr' => [
+                    'class' => 'form-control mt-4',
+                ],
+
+                'label' => 'City',
+                'label_attr' => ['class' => 'form-label mt-4']
+            ])
+            ->addEventListener(
+                FormEvents::PRE_SUBMIT,
+                [$this, 'onPreSubmit']
+            )
+
+            ->add('save', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary mt-4']
+            ])
+            ->getForm();
     }
     public function onPreSubmit(FormEvent $event)
     {
         $input = $event->getData()['city'];
-        $event->getForm()->add('city', ChoiceType::class,
-            ['choices' => [$input]]);
+        $event->getForm()->add(
+            'city',
+            ChoiceType::class,
+            ['choices' => [$input]]
+        );
     }
-   
 }
